01fb4ef77537a8c69ff4f566ed96b0bb26d5e8ae
volard <kirillzhilenkov@gmail.com>
2024-10-25 03:55:05 +0800

    :sparkles: use RecycleView

--- app/src/main/java/com/liberty/turnovermanagement/customers/list/CustomersFragment.java
+++ app/src/main/java/com/liberty/turnovermanagement/customers/list/CustomersFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.liberty.turnovermanagement.R;
@@ -69,6 +70,7 @@ public class CustomersFragment extends Fragment {
 
         adapter = new CustomerAdapter(this::openCustomerDetailsActivity);
         binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         viewModel.getCustomers().observe(getViewLifecycleOwner(), this::updateCustomerList);
 
--- app/src/main/java/com/liberty/turnovermanagement/orders/list/OrdersFragment.java
+++ app/src/main/java/com/liberty/turnovermanagement/orders/list/OrdersFragment.java
@@ -17,6 +17,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.liberty.turnovermanagement.R;
@@ -70,6 +71,7 @@ public class OrdersFragment extends Fragment {
 
         adapter = new OrderAdapter(this::openOrderDetailsActivity);
         binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         viewModel.getOrders().observe(getViewLifecycleOwner(), this::updateOrderList);
 
